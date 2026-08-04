8d80e933b11ce3756837678813a5273c4edab1d0
AKSHAY A <akshay890456@gmail.com>
2026-08-04 18:37:54 +0530

    Time: 19 ms (83.69%) | Memory: 45.8 MB (46.97%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -5,10 +5,12 @@ class Solution {
         int n = nums.length;
 
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue;
 
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if (j > i + 1 && nums[j] == nums[j - 1]) 
+                    continue;
 
                 int left = j + 1;
                 int right = n - 1;
